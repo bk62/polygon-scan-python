9d70d44a13f725ee7a1cee58b088cf6108ea4e93
bk62 <bkafle862@gmail.com>
2022-04-09 17:02:31 +0545

    add retry limit (e.g. 1 req / 2s) dataclass

---
+++ polygon_scan/http_client/datatypes.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class RateLimit:
+    """`limit` calls per `limit_time_interval` seconds"""
+
+    limit: int
+    limit_time_interval: int
+
+    def __str__(self) -> str:
+        return f"{self.limit} calls / {self.limit_time_interval}s"
