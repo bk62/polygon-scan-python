b79c688ef0c36261ca9aa8c100fa324cf4f1ab97
bk62 <bkafle862@gmail.com>
2022-04-10 10:41:19 +0545

    add a response class

--- polygon_scan/datatypes.py
+++ polygon_scan/datatypes.py
@@ -1,3 +1,6 @@
+from .exceptions import ClientException
+
+
 class AttrDict:
     def __init__(self, dictionary, **kwargs):
         self.__dict__ = dict(dictionary)
@@ -20,3 +23,34 @@ class AttrDict:
 
     def __repr__(self):
         return self.__class__.__name__ + f"({self.__dict__!r})"
+
+
+class APIResponse:
+    def __init__(self, response_dict, request_kwargs=None, request_url=None) -> None:
+        self.status = response_dict["status"]
+        self.message = response_dict["message"]
+
+        self.request_kwargs = request_kwargs or {}
+        self.request_url = request_url or ""
+
+        self._set_result(response_dict["result"])
+
+    def _set_result(self, result):
+        if type(result) == dict:
+            self._result = AttrDict(result)
+        elif type(result) == list:
+            self._result = [AttrDict(r) for r in result]
+        elif type(result) == str:
+            self._result = [result]
+        else:
+            raise ClientException("Unknown API result type/format", result)
+
+    @property
+    def result(self):
+        return self._result
+
+    def __len__(self):
+        return len(self._result)
+
+    def __getitem__(self, ix):
+        return self._result[ix]
